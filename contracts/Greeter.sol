bb429e580f29fe78ec4460411311557578c1581b
Issei Mori <isseieikisouya@outlook.jp>
2022-12-03 01:07:13 +0900

    privateなコントラクトないの変数定義

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -1,7 +1,11 @@
 pragma solidity 0.5.16;
 
 contract Greeter {
+    string private _greeting;
     function greet() external pure returns(string memory) {
         return "Hello World!";
+    }
+    function setGreeting(string calldata greeting) external {
+        _greeting = greeting;
     }
 }
