31e683ef201a9461cb2a1f1313569c214c328b35
Issei Mori <isseieikisouya@outlook.jp>
2022-12-03 01:46:14 +0900

    所有権の概念

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -8,10 +8,18 @@ contract Greeter {
         _owner = msg.sender;
     }
 
+    modifier onlyOwner() {
+        require(
+            msg.sender == _owner,
+            'Ownable: caller is not the owner'
+        );
+        _;
+    }
+
     function greet() external view returns(string memory) {
         return _greeting;
     }
-    function setGreeting(string calldata greeting) external {
+    function setGreeting(string calldata greeting) external onlyOwner {
         _greeting = greeting;
     }
     function owner() public view returns(address) {
